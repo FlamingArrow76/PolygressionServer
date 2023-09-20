046308aee90e781fd070e65afbcef85bf419d9af
FlamingArrow76 <110426254+FlamingArrow76@users.noreply.github.com>
2023-09-20 17:48:57 +0100

    Changed localhost to 0.0.0.0

--- RegressionServer.py
+++ RegressionServer.py
@@ -147,7 +147,7 @@ def equationfinder(x,y, xpower):
     return AverageError,p.tolist() 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 
